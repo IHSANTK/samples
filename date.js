b997c94cf618f91a33b5e3c930e31530be68a236
MUHAMMAD IHSAN TK <150010046+IHSANTK@users.noreply.github.com>
2024-05-16 12:00:00 +0000

    Commit for 2024-05-16T12:00:00

--- date.js
+++ date.js
@@ -21,7 +21,7 @@ function gitCommand(command) {
 function generateDates() {
     const dates = [];
     const months = [5]; // April and May
-    const days = [22,28];
+    const days = [16];
 
     for (const month of months) {
         for (const day of days) {
